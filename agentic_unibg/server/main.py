f6ccca0481eee176ecf3a89fe86da23e4b208cf5
Luca10 <l.brugnetti3@studenti.unibg.it>
2026-02-25 17:08:41 +0100

    Update main.py

--- agentic_unibg/server/main.py
+++ agentic_unibg/server/main.py
@@ -48,7 +48,8 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"],
+    allow_origin_regex=r"http://192\.168\.\d+\.\d+:\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
